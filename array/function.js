5b3d93ae8c52b86307208ae2a73e5367963e2b8c
yo1030 <0vant1c@gmail.com>
2022-08-07 10:01:28 +0900

    find/findIndex

--- array/function.js
+++ array/function.js
@@ -4,8 +4,22 @@ const numArrEmpty = [ 1, 2,, 4, 5];
 const stringArr = ["a", "b", "c", "d", "e"];
 const forEachBtn = document.getElementById('forEachBtn');
 const mapBtn = document.getElementById('mapBtn');
+const filterBtn = document.getElementById('filterBtn');
+const findBtn = document.getElementById('findBtn');
+const findIdxBtn = document.getElementById('findIdxBtn');
+const everyBtn = document.getElementById('everyBtn');
+const someBtn = document.getElementById('someBtn');
+const reduceBtn = document.getElementById('reduceBtn');
+const flatBtn = document.getElementById('flatBtn');
 let sum = 0;
 
+const makeArr = (num => {
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr[i] = i;    
+  }
+  return arr; 
+});
 const sampleArrForEach = (() => {
   sampleArr.forEach((element, i) => {
     if (!element.checked) return;
@@ -41,7 +55,7 @@ const sampleArrMap = (() => {
         break;
     }
   });
-})
+});
 
 const outputLogPlusForEach = (arr => {
   const copyArr = [...arr];
@@ -55,7 +69,7 @@ const outputLogPlusForEach = (arr => {
   console.log(copyArr);
   console.log(sum);
   sum = 0;
-})
+});
 const outputLogPlusMap = (arr => {
   const copyArr = [...arr];
   let tempElem = null;
@@ -69,6 +83,52 @@ const outputLogPlusMap = (arr => {
   console.log(newArr);
   console.log(sum);
   sum = 0;
+});
+const filterLog = (() => {
+  const mixArr = [1,2,"a", true, false, null, undefined];
+  const result1 = numArr.filter(x => x < 3);
+  const result2 = numArrEmpty.filter(() => true);
+  const result3 = mixArr.filter(x => x !== undefined && x !== null);
+  const result4 = mixArr.filter(x => x);
+  console.log(result1);
+  console.log(result2);
+  console.log(result3);
+  console.log(result4);
+});
+const findLog = (() => {
+  const arr = makeArr(20);
+  console.log(arr);
+  const result1 = arr.find(x => x === 7);
+  const result2 = arr.find(x => x % 10 === 0);
+  const result3 = arr.find(x => x > 5);
+  const result4 = arr.find(x => x > 21);
+  console.log(result1);
+  console.log(result2);
+  console.log(result3);
+  console.log(result4);
+});
+const findIdxLog = (() => {
+  const arr = makeArr(20);
+  console.log(arr);
+  const result1 = arr.findIndex(x => x === 7);
+  const result2 = arr.findIndex(x => x % 10 === 0);
+  const result3 = arr.findIndex(x => x > 5);
+  const result4 = arr.findIndex(x => x > 21);
+  console.log(result1);
+  console.log(result2);
+  console.log(result3);
+  console.log(result4);
+});
+const everyLog = (() => {
+  console.log('every');
+})
+const someLog = (() => {
+  console.log('some');
 })
 forEachBtn.addEventListener('click', sampleArrForEach);
 mapBtn.addEventListener('click', sampleArrMap);
+filterBtn.addEventListener('click', filterLog);
+findBtn.addEventListener('click', findLog);
+findIdxBtn.addEventListener('click', findIdxLog);
+everyBtn.addEventListener('click', everyLog);
+someBtn.addEventListener('click', someLog);
