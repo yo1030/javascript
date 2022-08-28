e8a2ece1430d10432a3598c24611a73bff20888b
yo1030 <0vant1c@gmail.com>
2022-08-28 14:41:14 +0900

    concat

--- array/concat.js
+++ array/concat.js
@@ -1,15 +1,13 @@
 const concatBtn = document.getElementById('concatBtn');
 const concatNestBtn = document.getElementById('concatNestBtn');
-// 元の配列は変更されない
-// 新しい配列が作成される
+/** https://honobonoengineer.com/js-concat/ */
 const concatLog = (() => {
   const arr = [1,"a",2];
   const arr1 = arr.concat([3,"b"]);
   console.log(arr);
   console.log(arr1);
 });
-// 引数の配列は展開される
-// 配列の配列は展開されない
+/** https://honobonoengineer.com/js-concat/ */
 const concatNestLog = (() => {
   const arr = [1, 2, 3];
   const arr1 = arr.concat([4, 5], [6, 7]);
