5baf898491b4a932b501296dea2e14df1060b790
yo1030 <0vant1c@gmail.com>
2022-10-09 10:14:12 +0900

    add component

--- vue/sec6/vue2_vue3/src/App.vue
+++ vue/sec6/vue2_vue3/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <TestComponent />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './modules/TestComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TestComponent
   }
 }
 </script>
