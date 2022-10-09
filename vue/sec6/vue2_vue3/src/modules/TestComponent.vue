43f4e75902780040c9984ad2741296cdb67c4889
yo1030 <0vant1c@gmail.com>
2022-10-09 10:14:24 +0900

    add style

---
+++ vue/sec6/vue2_vue3/src/modules/TestComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="b-red">
+    テストです
+    {{ testData }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data(){
+    return {
+      testData: 'テストdataです'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .b-red {
+    border: 1px red solid;
+  }
+</style>
