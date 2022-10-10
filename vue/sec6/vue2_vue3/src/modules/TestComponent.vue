86325ea113d5fee6ef18869d710c354fbc5eb9ef
yo1030 <0vant1c@gmail.com>
2022-10-10 10:58:20 +0900

    vue multipage, scss

--- vue/sec6/vue2_vue3/src/modules/TestComponent.vue
+++ vue/sec6/vue2_vue3/src/modules/TestComponent.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="b-red">
+    <div class="b-blue">
+      SCSSのテストです
+    </div>
+    <div class="background_primary">
+      SCSSグローバルテスト
+    </div>
     テストです
     {{ testData }}
   </div>
@@ -16,8 +22,16 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   .b-red {
     border: 1px red solid;
+  }
+  .b {
+    &-blue {
+      border: 1px blue solid;
+    }
+  }
+  .background_primary{
+    background-color: $color-primary;
   }
 </style>
