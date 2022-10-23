d3f294a3a7f410af289c0dd744f3999f4d314533
yo1030 <0vant1c@gmail.com>
2022-10-23 15:37:40 +0900

    bookapp

---
+++ vue/sec8/bookapp/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import BookEdit from '@/pages/BookEdit'
+import BookIndex from '@/pages/BookIndex'
+import BookSearch from '@/pages/BookSearch'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'BookIndex',
+    component: BookIndex
+  },
+  {
+    path: '/search',
+    name: 'BookSearch',
+    component: BookSearch
+  },
+  {
+    path: '/edit/:id',
+    name: 'BookEdit',
+    component: BookEdit
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
