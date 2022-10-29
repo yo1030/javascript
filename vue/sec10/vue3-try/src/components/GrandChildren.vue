c81eb80aadc49272a0dff207212807e2b6c70595
yo1030 <0vant1c@gmail.com>
2022-10-29 12:43:51 +0900

    vue3-provide-inject

---
+++ vue/sec10/vue3-try/src/components/GrandChildren.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    GrandChildren
+  </div>
+  <div>{{userName}}</div>
+</template>
+
+<script>
+export default {
+  data(){},
+  inject: ['userName']
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/sec10/vue3-try/src/views/ChildrenView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Children
+    <Grand-Children />
+  </div>
+</template>
+
+<script>
+import GrandChildren from "@/components/GrandChildren";
+export default {
+  components: {
+    GrandChildren
+  }
+}
+</script>
+
+<style>
+
+</style>
